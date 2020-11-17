a251cdffe2a91a35e64e82978cb7b97f1c8bb051
Fahd Arhalai <fahd.arh@gmail.com>
2020-11-17 18:23:04 +0100

    AutoInject annotation for dependency injection

--- src/main/java/jioc/annotation/AutoInject.java
+++ src/main/java/jioc/annotation/AutoInject.java
@@ -1,2 +1,19 @@
-package jioc.annotation;public class AutoInject {
+package jioc.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marks a field as to be auto injected by the JIOC dependency injection mechanism.
+ * The annotation is simple to use and requires no parameters at all.
+ *
+ * @Author: Fahd Arhalai
+ * @see jioc.annotation.Pea
+ * @see jioc.configurer.AnnotationApplicationConfig
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface AutoInject {
 }
