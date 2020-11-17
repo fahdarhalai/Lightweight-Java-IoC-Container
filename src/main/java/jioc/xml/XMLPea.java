599ba02b0dce283a717cadfc6e60fde741fa1230
Fahd Arhalai <fahd.arh@gmail.com>
2020-11-17 18:25:47 +0100

    Data structure for XML-based config

--- src/main/java/jioc/xml/XMLPea.java
+++ src/main/java/jioc/xml/XMLPea.java
@@ -5,113 +5,76 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- * @param <T>
+ * The main data structure for storing the XML pea with its corresponding fields and attributes.
+ *
+ * @see jioc.configurer.XMLApplicationConfig
  */
-public class XMLChickpea<T> {
-    /**
-     *
-     */
+public class XMLPea<T> {
     private String id;
-    /**
-     *
-     */
     private Class<T> c;
-    /**
-     *
-     */
     private String initMethod;
-    /**
-     *
-     */
     private HashMap<String, String> fields;
 
     /**
+     * Default constructor for the XMLPea data structure.
      *
+     * @see jioc.configurer.XMLApplicationConfig
      */
-    public XMLChickpea() {
+    public XMLPea() {
         this.fields = new HashMap<String, String>();
     }
 
     /**
-     * @param id
-     * @param c
+     * Constructor with arguments for the XMLPea data structure.
+     *
+     * @param id unique identifier attribute specified in the XML pea tag.
+     * @param c Class object representing the class of the pea in a running Java application.
+     * @see jioc.configurer.XMLApplicationConfig
+     * @see java.lang.Class#forName(String)
      */
-    public XMLChickpea(String id, Class c) {
+    public XMLPea(String id, Class c) {
         this.id = id;
         this.c = c;
         this.fields = new HashMap<String, String>();
     }
 
-    /**
-     * @return
-     */
     public String getId() {
         return id;
     }
 
-    /**
-     * @return
-     */
     public Class<T> getC() {
         return c;
     }
 
-    /**
-     * @return
-     */
     public HashMap<String, String> getFields() {
         return fields;
     }
 
-    /**
-     * @param id
-     */
     public void setId(String id) {
         this.id = id;
     }
 
-    /**
-     * @param c
-     */
     public void setC(Class<T> c) {
         this.c = c;
     }
 
-    /**
-     * @return
-     */
     public String getInitMethod() {
         return initMethod;
     }
 
-    /**
-     * @param initMethod
-     */
     public void setInitMethod(String initMethod) {
         this.initMethod = initMethod;
     }
 
-    /**
-     * @param name
-     * @param ref
-     */
     public void addField(String name, String ref){
         this.fields.put(name, ref);
     }
 
-    /**
-     * @param name
-     * @return
-     */
     public String getReferenceByField(String name){
         if(this.fields == null || this.fields.isEmpty()) return null;
         return this.fields.get(name);
     }
 
-    /**
-     * @param ref
-     * @return
-     */
     public List<String> getFieldsByReference(String ref){
         if(this.fields == null || this.fields.isEmpty()) return null;
 
