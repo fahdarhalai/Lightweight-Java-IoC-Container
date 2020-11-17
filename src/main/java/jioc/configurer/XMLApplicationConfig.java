88e9a58dd21ce515974ab073df91ddaa88fc1795
Fahd Arhalai <fahd.arh@gmail.com>
2020-11-17 18:26:43 +0100

    XML-based configuration implementation

--- src/main/java/jioc/configurer/XMLApplicationConfig.java
+++ src/main/java/jioc/configurer/XMLApplicationConfig.java
@@ -1,7 +1,6 @@
-package jioc.xml.configurer;
+package jioc.configurer;
 
-import jioc.ApplicationConfig;
-import jioc.xml.chickpeas.XMLChickpea;
+import jioc.xml.XMLPea;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -17,11 +16,36 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicReference;
 
+/**
+ * Implementation of {@link jioc.configurer.ApplicationConfig} interface providing XML-based configuration.
+ * Uses a custom XML file which declares the peas to be managed by the JIOC framework.
+ *
+ * Example:
+ * <pre>
+ * {@code
+ * <pea id="instance_of_B" class="com.example.B" init-method="initB"></pea>
+ * <pea id="instance_of_A" class="com.example.A" init-method="initA">
+ *      <field name="field_B" ref="instance_of_B"></field>
+ * </pea>
+ * }
+ * </pre>
+ *
+ * NOTE: 'id' and 'class' attributes are required while 'init-method' is optional and depends on the business logic.
+ */
 public class XMLApplicationConfig<T> implements ApplicationConfig {
     private File config;
     private Document document;
-    private HashMap<String, XMLChickpea<T>> chickpeas;
+    private HashMap<String, XMLPea<T>> peas;
 
+    /**
+     * Create a new {@link jioc.configurer.XMLApplicationConfig} which takes a string denoting the path to the XML configuration file to be scanned for components to be managed by the framework.
+     *
+     * @param s path to XML configuration file.
+     * @throws ParserConfigurationException
+     * @throws ClassNotFoundException
+     * @throws IOException
+     * @throws SAXException
+     */
     public XMLApplicationConfig(String s) throws ParserConfigurationException, ClassNotFoundException, IOException, SAXException {
         this.config = new File(s);
         this.document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(this.config);
@@ -29,8 +53,8 @@ public class XMLApplicationConfig<T> implements ApplicationConfig {
     }
 
     private void init() throws ClassNotFoundException {
-        this.chickpeas = new HashMap<String, XMLChickpea<T>>();
-        NodeList nodes = this.document.getElementsByTagName("chickpea");
+        this.peas = new HashMap<String, XMLPea<T>>();
+        NodeList nodes = this.document.getElementsByTagName("pea");
 
         for(int i=0; i<nodes.getLength(); i++){
             Node node = nodes.item(i);
@@ -41,11 +65,11 @@ public class XMLApplicationConfig<T> implements ApplicationConfig {
                 String id = nElement.getAttribute("id");
                 String className = nElement.getAttribute("class");
                 Class c = Class.forName(className);
-                XMLChickpea chickpea = new XMLChickpea(id, c);
+                XMLPea pea = new XMLPea(id, c);
 
                 String initMethod = nElement.getAttribute("init-method");
-                if(initMethod != null && initMethod != ""){
-                    chickpea.setInitMethod(initMethod);
+                if(initMethod != null && !initMethod.equals("")){
+                    pea.setInitMethod(initMethod);
                 }
 
                 NodeList fields = nElement.getElementsByTagName("field");
@@ -57,30 +81,30 @@ public class XMLApplicationConfig<T> implements ApplicationConfig {
                         String fieldName = eField.getAttribute("name");
                         String ref = eField.getAttribute("ref");
 
-                        chickpea.addField(fieldName, ref);
+                        pea.addField(fieldName, ref);
                     }
                 }
 
-                this.chickpeas.put(id, chickpea);
+                this.peas.put(id, pea);
             }
         }
     }
 
-    public <T> T instantiateChickpea(XMLChickpea<T> chickpea) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        T obj = chickpea.getC().getDeclaredConstructor().newInstance();
+    private <T> T instantiatePea(XMLPea<T> pea) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        T obj = pea.getC().getDeclaredConstructor().newInstance();
 
-        chickpea.getFields().forEach((k,v) -> {
-            XMLChickpea field = this.chickpeas.get(v);
+        pea.getFields().forEach((k,v) -> {
+            XMLPea field = this.peas.get(v);
             try {
-                Object fieldObj = instantiateChickpea(field);
+                Object fieldObj = instantiatePea(field);
 
                 String setterName = "set" + k.substring(0,1).toUpperCase() + k.substring(1);
-                Method setterMethod = chickpea.getC().getMethod(setterName, field.getC().getInterfaces()[0]);
+                Method setterMethod = pea.getC().getMethod(setterName, field.getC().getInterfaces()[0]);
                 setterMethod.invoke(obj, field.getC().getInterfaces()[0].cast(fieldObj));
 
-                String initName = chickpea.getInitMethod();
+                String initName = pea.getInitMethod();
                 if(initName != null && initName != "") {
-                    Method initMethod = chickpea.getC().getMethod(initName);
+                    Method initMethod = pea.getC().getMethod(initName);
                     initMethod.invoke(obj);
                 }
             } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
@@ -91,14 +115,21 @@ public class XMLApplicationConfig<T> implements ApplicationConfig {
         return obj;
     }
 
+    /**
+     * Return an instance of the class that implements the interface.
+     * There should be no ambiguity when the JIOC scanning mechanism looks for an implementation registered using XML pea tags.
+     *
+     * @param classObject interface type the pea must implement.
+     * @return an instance of the pea.
+     */
     @Override
-    public <T> T getChickpea(Class<T> classObject) {
+    public <T> T getPea(Class<T> classObject) {
         AtomicReference<T> out = new AtomicReference<>();
 
-        this.chickpeas.forEach((k,v) -> {
+        this.peas.forEach((k, v) -> {
             if(classObject.isAssignableFrom(v.getC())){
                 try {
-                    out.set((T) instantiateChickpea(v));
+                    out.set((T) instantiatePea(v));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
