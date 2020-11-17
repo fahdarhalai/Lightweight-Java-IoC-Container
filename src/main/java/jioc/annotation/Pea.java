68751e5cd3bdbbef5c1e390e42af571dc8d559bf
Fahd Arhalai <fahd.arh@gmail.com>
2020-11-17 18:24:18 +0100

    Pea annotation for auto-detection

--- src/main/java/jioc/annotation/Pea.java
+++ src/main/java/jioc/annotation/Pea.java
@@ -6,10 +6,16 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * Indicates that the annotated class should be managed by the JIOC framework and is considered as a candidate for auto-detection when using annotation-based configuration.
+ * Accepts an optional initMethod parameter specifying the logic to run right after a constructor call to the annotated class.
  *
+ * @Author: Fahd Arhalai
+ * @see jioc.annotation.AutoInject
+ * @see jioc.annotation.AnnotatedPea
+ * @see jioc.configurer.AnnotationApplicationConfig
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-public @interface Chickpea {
+public @interface Pea {
     public String initMethod() default "";
 }
